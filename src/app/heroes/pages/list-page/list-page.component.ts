dcd27608cb694c1b34e33049fdc8b0d9097e260f
Emanuel Saenz <emanuel_saenz99@outlook.com>
2024-11-08 17:35:11 -0600

    Primer commit funcional

---
+++ src/app/heroes/pages/list-page/list-page.component.ts
@@ -0,0 +1,20 @@
+import { Hero } from '../../interfaces/hero.interface';
+import { HeroesService } from './../../services/heroes.service';
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-page',
+  templateUrl: './list-page.component.html',
+  styles: ``
+})
+export class ListPageComponent implements OnInit {
+
+  public heroes: Hero[] = [];
+
+  constructor( private HeroesService: HeroesService ) { }
+
+  ngOnInit(): void{
+    this.HeroesService.getHeroes()
+      .subscribe( heroes => this.heroes = heroes );
+  }
+}
